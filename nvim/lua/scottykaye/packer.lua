cb406d9144867e688427b68d42ee05275637093e
Scott Kaye <scottykaye.web@gmail.com>
2024-09-19 10:49:47 -0400

    change bindings

--- nvim/lua/scottykaye/packer.lua
+++ nvim/lua/scottykaye/packer.lua
@@ -172,31 +172,32 @@ return require('packer').startup(function(use)
 
   use({
     'monkoose/neocodeium',
-    event = 'InsertEnter', -- Replacing "VeryLazy" with a relevant event for packer
+    event = 'InsertEnter',
     config = function()
       local neocodeium = require("neocodeium")
       neocodeium.setup()
-      vim.keymap.set("i", "<A-f>", function()
+
+      -- Define keymaps for Neocodeium functions
+      vim.keymap.set("i", "<C-f>", function()
         require("neocodeium").accept()
       end)
-      vim.keymap.set("i", "<A-w>", function()
+      vim.keymap.set("i", "<C-w>", function()
         require("neocodeium").accept_word()
       end)
-      vim.keymap.set("i", "<A-a>", function()
+      vim.keymap.set("i", "<C-a>", function()
         require("neocodeium").accept_line()
       end)
-      vim.keymap.set("i", "<A-e>", function()
+      vim.keymap.set("i", "<C-e>", function()
         require("neocodeium").cycle_or_complete()
       end)
-      vim.keymap.set("i", "<A-r>", function()
+      vim.keymap.set("i", "<C-r>", function()
         require("neocodeium").cycle_or_complete(-1)
       end)
-      vim.keymap.set("i", "<A-c>", function()
+      vim.keymap.set("i", "<C-c>", function()
         require("neocodeium").clear()
       end)
     end
   })
-
   --  use({
   --    'Mofiqul/dracula.nvim',
   --    as = "dracula",
