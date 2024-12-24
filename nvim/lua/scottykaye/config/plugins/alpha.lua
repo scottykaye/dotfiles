65a953334ddb46f1efa603747700bf175301445f
Scott Kaye <scottykaye.web@gmail.com>
2024-12-24 09:03:23 -0500

    Add Avante and add some styling tweaks

---
+++ nvim/lua/scottykaye/config/plugins/alpha.lua
@@ -0,0 +1,24 @@
+local alpha = require("alpha")
+local dashboard = require("alpha.themes.dashboard")
+
+math.randomseed(os.time())
+local function pick_color()
+	local colors = { "String", "Identifier", "Keyword", "Number" }
+	return colors[math.random(#colors)]
+end
+
+local logo = {
+	"                                                     ",
+	"  ███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗ ",
+	"  ████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║ ",
+	"  ██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║ ",
+	"  ██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║ ",
+	"  ██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║ ",
+	"  ╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝ ",
+	"                                                     ",
+}
+
+dashboard.section.header.val = logo
+dashboard.section.header.opts.hl = pick_color()
+
+alpha.setup(dashboard.opts)
