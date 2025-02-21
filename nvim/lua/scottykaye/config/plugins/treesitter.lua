899f5d76d93df8a1d9e41f8e95ef49c796b2684b
ScottyKaye <scott.kaye@tollbit.com>
2025-02-21 13:51:05 -0500

    Add Comment shortcuts

--- nvim/lua/scottykaye/config/plugins/treesitter.lua
+++ nvim/lua/scottykaye/config/plugins/treesitter.lua
@@ -1,19 +1,12 @@
 require 'nvim-treesitter.configs'.setup {
-  -- A list of parser names, or "all" (the five listed parsers should always be installed)
-  ensure_installed = { "javascript", "html", "typescript", "markdown", "markdown_inline", "go", "rust", "c", "lua",
-    "vim", "vimdoc", "query" },
-  -- Install parsers synchronously (only applied to `ensure_installed`)
+  ensure_installed = {
+    "javascript", "html", "typescript", "markdown", "markdown_inline",
+    "go", "rust", "c", "lua", "vim", "vimdoc", "query"
+  },
   sync_install = false,
-  -- Automatically install missing parsers when entering buffer
-  -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
   auto_install = true,
   highlight = {
     enable = true,
-
-    -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-    -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
-    -- Using this option may slow down your editor, and you may see some duplicate highlights.
-    -- Instead of true it can also be a list of languages
     additional_vim_regex_highlighting = false,
   },
 }
@@ -26,7 +19,17 @@ vim.filetype.add({
 
 require('treesitter-context').setup({})
 
-
--- think these lines are borked
--- local ft_to_parser = require("nvim-treesitter.parsers").filetype_to_parsername
--- ft_to_parser.mdx = "markdown"
+require('ts_context_commentstring').setup {
+  enable_autocmd = false,
+  pre_hook = require('ts_context_commentstring.integrations.comment_nvim').create_pre_hook(),
+  config = {
+    javascript = {
+      __default = '// %s',
+      jsx_element = '{/* %s */}',
+      jsx_fragment = '{/* %s */}',
+      jsx_attribute = '// %s',
+      comment = '// %s',
+    },
+    typescript = { __default = '// %s', __multiline = '/* %s */' },
+  },
+}
