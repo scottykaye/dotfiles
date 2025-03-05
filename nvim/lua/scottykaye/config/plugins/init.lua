615d4702cf9af4a815d9cd23642f8c46eba8a8bb
ScottyKaye <scott.kaye@tollbit.com>
2025-03-05 08:57:20 -0500

    Add Gitsigns change

--- nvim/lua/scottykaye/config/plugins/init.lua
+++ nvim/lua/scottykaye/config/plugins/init.lua
@@ -64,11 +64,11 @@ return {
 
   {
     "nvim-lua/plenary.nvim",
-    lazy = false,
-    priority = 1000,
+    lazy = true,
   },
   {
     "lewis6991/gitsigns.nvim",
+    event = { "BufReadPre", "BufNewFile" },
     dependencies = { "nvim-lua/plenary.nvim" },
     config = function()
       require("scottykaye.config.plugins.gitsigns")
