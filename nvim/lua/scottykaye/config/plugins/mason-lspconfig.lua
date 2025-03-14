27692e33c95af8c86f6821f41f0f92af95cdb306
ScottyKaye <scott.kaye@tollbit.com>
2025-03-14 14:39:56 -0400

    Try some new thigns

--- nvim/lua/scottykaye/config/plugins/mason-lspconfig.lua
+++ nvim/lua/scottykaye/config/plugins/mason-lspconfig.lua
@@ -1,3 +1,3 @@
 require("mason-lspconfig").setup({
-	ensured_installed = { "lua_ls", "solargraph", "ts_ls", "biome", "eslint", "tailwindcss" },
+  ensured_installed = { "lua_ls", "solargraph", "ts_ls", "biome", "eslint", "tailwindcss", "phpactor", "php-cs-fixer" },
 })
