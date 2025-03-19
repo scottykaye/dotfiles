8f81870c87a10666f6ab34d9352afe3373ce7f1c
ScottyKaye <scott.kaye@tollbit.com>
2025-03-19 14:03:36 -0400

    updats

--- nvim/lua/set.lua
+++ nvim/lua/set.lua
@@ -1,7 +1,6 @@
 -- vim.opt.guicursor = ""
 vim.opt.number = true
--- vim.opt.nu = true
-vim.opt.relativenumber = true
+-- vim.opt.relativenumber = true
 
 vim.opt.tabstop = 2
 vim.opt.softtabstop = 2
